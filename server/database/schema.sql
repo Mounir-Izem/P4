3a149a11c89eac66dbefc78ae4b61f8469599528
Mounir <izem.mounir@gmail.com>
2025-04-02 10:15:58 +0100

    Configuration de l'environnement de la bdd

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,42 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+CREATE TABLE categories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(100) NOT NULL,
+    description TEXT
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE manufacturer (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(100) NOT NULL,
+    pays VARCHAR(100) NOT NULL
 );
 
-insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
+CREATE TABLE weapons (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(100) NOT NULL,
+    description TEXT,
+    calibre VARCHAR(50),
+    poids DECIMAL(5,2),
+    longueur DECIMAL(5,2),
+    categorie_id INT,
+    fabricant_id INT,
+    date_de_fabrication YEAR,
+    CONSTRAINT fk_categorie FOREIGN KEY (categorie_id) REFERENCES categories(id),
+    CONSTRAINT fk_fabricant FOREIGN KEY (fabricant_id) REFERENCES fabricants(id)
+);
 
-insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+INSERT INTO categories (nom, description)
+VALUES 
+('Armes à feu', 'Armes qui utilisent de la poudre pour projeter un projectile'),
+('Armes blanches', 'Armes tranchantes ou contondantes, généralement sans mécanisme de tir');
+
+INSERT INTO manufacturer (nom, pays)
+VALUES 
+('Colt', 'USA'),
+('Beretta', 'Italie'),
+('Heckler & Koch', 'Allemagne');
+
+INSERT INTO weapons (nom, description, calibre, poids, longueur, categorie_id, fabricant_id, date_de_fabrication)
+VALUES 
+('M16', 'Fusil d’assaut américain, utilisé principalement par l’armée', '5.56 mm', 3.5, 100, 1, 1, 1964),
+('AK-47', 'Fusil d’assaut soviétique, largement utilisé dans le monde', '7.62 mm', 4.3, 87, 1, 2, 1947),
+('Katana', 'Épée japonaise traditionnelle, réputée pour sa lame courbée', NULL, 1.2, 100, 2, NULL, 1600);
