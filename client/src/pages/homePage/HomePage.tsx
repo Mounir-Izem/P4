33d81541fb09fefae88276281288ff5f38ea7784
Mounir <izem.mounir@gmail.com>
2025-04-03 16:46:11 +0100

    Début de la homepage

--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -3,7 +3,17 @@ import "./HomePage.css";
 export default function HomePage() {
   return (
     <>
-      <main>Welcome</main>
+      <main>
+        <div className="hero-container">
+          <h1 className="title-hero">Bienvenue dans ton inventaire</h1>
+        </div>
+        <section>
+          <div className="container-new">
+            <h2>Les nouveautés</h2>
+            <div className="bar" />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
